5b6acfa794ff5a2a2f92a230384bee2759603541
Vladislav Tsendrovskii <vlad@vltc.net.eu.org>
2025-05-24 20:32:57 +0200

    Make periods proportional to prime numbers to prevent resonances

--- core.c
+++ core.c
@@ -6,12 +6,15 @@
 
 #define max(a,b) ((a) > (b) ? (a) : (b))
 
+// 3 prime numbers to prevent astigmatism
+static int X_T      = 11; // X carriage period proportional
+static int MIRROR_T = 37; // Mirror rotation period proportional
+static int TABLE_T  = 47; // Table rotation period proportional
+
 // Parameters
-static int TABLE_PERIOD = 43;  // Period of table rotation in seconds
-static int MIRROR_PERIOD = 55; // Period of mirror rotation in seconds
-static int X_SPEED = 30;       // Speed of X movement, mm/seconds
-static float X_AMPL_1 = 15.0;  // Amplitude of X movements in mm
-static float X_AMPL_2 = 15.0;  // Amplitude of X movements in mm
+static float X_SPEED = 30.0;   // X speed in mm/s
+static float X_AMPL_1 = 0.0;   // Amplitude of X movements in mm
+static float X_AMPL_2 = 0.0;   // Amplitude of X movements in mm
 
 // Calculated parameters
 static uint32_t table_delay_us;
@@ -40,20 +43,32 @@ enum core_state_e
 // Config functiions
 void core_calculate_parameters(void)
 {
-    float table_steps_per_rot = (float)TABLE_STEPPER_STEPS * TABLE_STEPPER_MICRO * TABLE_REDUCER_BIG / TABLE_REDUCER_SMALL;
-    table_delay_us = 1e6 * TABLE_PERIOD / table_steps_per_rot;
-    table_timer = 0;
-    hw_table_set_dir(TABLE_DIR);
+    // full x path
+    float X_path = 2 * (X_AMPL_1 + X_AMPL_2);
+    float X_period = X_path / X_SPEED;
+    
+    // coefficient of proportion
+    float k = X_period / X_T;
 
-    float mirror_steps_per_rot = (float)MIRROR_STEPPER_STEPS * MIRROR_STEPPER_MICRO * MIRROR_REDUCER_BIG / MIRROR_REDUCER_SMALL;
-    mirror_delay_us = 1e6 * MIRROR_PERIOD / mirror_steps_per_rot;
-    mirror_timer = 0;
-    hw_mirror_set_dir(MIRROR_DIR);
-
+    // Calculate X carriage delay between steps
     float x_steps_per_mm = (float)X_STEPPER_STEPS * X_STEPPER_MICRO / (X_GEAR_NTEETH * X_GEAR_TOOTH);
     x_steps_1 = max(X_AMPL_1 * x_steps_per_mm, 1);
     x_steps_2 = max(X_AMPL_2 * x_steps_per_mm, 1);
     x_delay_us = 1e6 / (X_SPEED * x_steps_per_mm);
+
+    // Calculate rotary table delay between steps
+    float table_period = TABLE_T * k;
+    float table_steps_per_rot = (float)TABLE_STEPPER_STEPS * TABLE_STEPPER_MICRO * TABLE_REDUCER_BIG / TABLE_REDUCER_SMALL;
+    table_delay_us = max(1e6 * table_period / table_steps_per_rot, 1);
+    table_timer = 0;
+    hw_table_set_dir(TABLE_DIR);
+
+    // Calculate mirror rotator delay between steps
+    float mirror_period = MIRROR_T * k;
+    float mirror_steps_per_rot = (float)MIRROR_STEPPER_STEPS * MIRROR_STEPPER_MICRO * MIRROR_REDUCER_BIG / MIRROR_REDUCER_SMALL;
+    mirror_delay_us = max(1e6 * mirror_period / mirror_steps_per_rot, 1);
+    mirror_timer = 0;
+    hw_mirror_set_dir(MIRROR_DIR);
 }
 
 void core_set_amplitude_1(float ampl)
